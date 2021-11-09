45f1f5e8fdb601d86f4bca0d74b65e097c6df826
Cristian Torrejon <c.torrejon96@gmail.com>
2021-11-09 15:37:11 -0300

    Task 00 Responsable: Cristian Torrejon Descripcion: Se creo el proyecto Spring

---
+++ src/main/java/ar/edu/unju/fi/ProyectoTesteoCovid19Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTesteoCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTesteoCovid19Application.class, args);
+	}
+
+}
